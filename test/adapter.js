55e8ba756f5d3222dde7e465079cac11c60757a0
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2016-04-26 12:46:13 +0200

    Basic and Token auth handling finished

--- test/adapter.js
+++ test/adapter.js
@@ -119,12 +119,8 @@ describe('Swagger 2.0 adapter', () => {
   });
 
   describe('can parse fixtures', () => {
-    const fixtures = swaggerZoo.samples();
+    const fixtures = swaggerZoo.features();
     fixtures.forEach((fixture) => {
-      if (fixture.name.slice(0, 4) === 'auth') {
-        return;
-      }
-
       testFixture(`Parses ${fixture.name}`, fixture);
       testFixture(`Parses ${fixture.name} with source maps`, fixture, true);
     });
