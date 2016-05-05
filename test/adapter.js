493e664d3ef6a52d012fd959502bc2d5d633a417
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2016-05-05 15:34:23 +0200

    Fixed lint issues

--- test/adapter.js
+++ test/adapter.js
@@ -119,7 +119,7 @@ describe('Swagger 2.0 adapter', () => {
   });
 
   describe('can parse fixtures', () => {
-    const fixtures = swaggerZoo.features();
+    const fixtures = swaggerZoo.samples();
     fixtures.forEach((fixture) => {
       testFixture(`Parses ${fixture.name}`, fixture);
       testFixture(`Parses ${fixture.name} with source maps`, fixture, true);
